49ccadae33a7c403c53cef9f647d355ea76b121e
Julia Davis <juliakhdavis@gmail.com>
2022-03-12 19:44:38 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//passwordgenerator
+
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+‑/",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    document.getElementById("result").innerText = retVal
+
+    return retVal;
+}
+
+console.log(generatePassword())
